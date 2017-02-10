e3665c6307ccaa9bf5bf7e939b3e5ba0a7b85934
r-ogata <r-ogata@d1-ubuntu-01.p.office.bbtower.co.jp>
2017-02-10 11:49:29 +0900

    bug fix

--- script/R/fun/mxnetResult.R
+++ script/R/fun/mxnetResult.R
@@ -1,9 +1,9 @@
 mxnetResult <- function() {
   df <- data.frame(
-    test_ROC_1 = pROC::roc(test.y, preds_test[,1])$auc[1]
-    ,test_ROC_2 = pROC::roc(test.y, preds_test[,2])$auc[1]
-    ,train_ROC_1 = pROC::roc(train.y, preds_train[,1])$auc[1]
-    ,train_ROC_2 = pROC::roc(train.y, preds_train[,2])$auc[1]
+    test_ROC_1 = pROC::roc(TRAIN.TEST.PRED["response",], preds_train.test[,1])$auc[1]
+    ,test_ROC_2 = pROC::roc(TRAIN.TEST.PRED["response",], preds_train.test[,2])$auc[1]
+    ,train_ROC_1 = pROC::roc(TRAIN.TRAIN.PRED["response",], preds_train.train[,1])$auc[1]
+    ,train_ROC_2 = pROC::roc(TRAIN.TRAIN.PRED["response",], preds_train.train[,2])$auc[1]
     ,num.round = NUM.ROUND
     ,array.batch.size = ARRAY.BATCH.SIZE
     ,learning.rate = LEARNING.RATE
